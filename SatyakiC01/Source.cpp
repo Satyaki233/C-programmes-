cb67e169d1e6e4417605b06d062c585a62a0443f
Satyaki De Sarkar <satyakidesarkar2017@gmail.com>
2021-10-23 19:05:37 +0530

    code commit

---
+++ SatyakiC01/Source.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main() {
+
+	char name[] = "Satyaki De Sarkar";
+	int age = 21;
+	printf("%s is the best and he is only %d /n", name,age);
+	
+	
+	return 0;
+}
